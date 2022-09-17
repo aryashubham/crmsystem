98ff28100864da3b2c0fd4c10061e3194bb83860
Shubham <aryashubhammalikss@gmail.com>
2022-09-17 08:58:49 +0530

    leads model

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Register your models here
+@admin.register(MyUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email']
+    search_fields = ['email']
+
+
--- leads/models.py
+++ leads/models.py
@@ -1,9 +1,47 @@
 from django.db import models
-
-
-
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def _create_user(self,email,password, **extra_fields):
+        if not email:
+            raise ValueError(('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+    def create_user(self,email,password, **extra_fields):
+        extra_fields.setdefault('is_active', True)
+        return self._create_user(email,password,**extra_fields)
+
+    def create_superuser(self,email,password, **extra_fields):
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email,password,**extra_fields)
+
+
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=256, unique=True)
+    name = models.CharField(max_length=255)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self) -> str:
+        return self.email
+
+
 Source_Choice = (
     ('Facebook', 'Facebook'),
     ('Instagram', 'Instagram'),
@@ -14,7 +52,12 @@ class Lead(models.Model):
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
     age = models.IntegerField()
+    email = models.EmailField()
     phoned = models.BooleanField(default=False)
     source = models.CharField(choices=Source_Choice,max_length=250)
-    profile_picture = models.ImageField(blank=True, null=True)
-    special_file = models.FileField(null=True, blank=True)
+    agent = models.ForeignKey("Agent", models.SET_NULL, null=True)
+
+
+class Agent(models.Model):
+    user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
+    phone_number = PhoneNumberField()
